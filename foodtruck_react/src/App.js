abfcfe1c8ba622a21bd3af509145fd9e91ddd4ea
Hannah Zulueta <hannahjoy.zulueta@gmail.com>
2018-07-13 15:11:00 -0700

    Remove logo

--- foodtruck_react/src/App.js
+++ foodtruck_react/src/App.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
