8f4dd91826d7febbfdd5e39b3562c74b769468ea
Hannah Zulueta <hannahjoy.zulueta@gmail.com.com>
2018-07-13 15:33:31 -0700

    Install reactstrap

--- foodtruck_react/src/App.js
+++ foodtruck_react/src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <React.Fragment>hi!</React.Fragment>;
   }
 }
 
