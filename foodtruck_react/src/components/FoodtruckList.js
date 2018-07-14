3ffa1e24b93b769f63c660241419a72f50dd4eaa
Hannah Zulueta <hannahjoy.zulueta@gmail.com.com>
2018-07-14 11:20:21 -0700

    Create foodtruck list component

--- foodtruck_react/src/components/FoodtruckList.js
+++ foodtruck_react/src/components/FoodtruckList.js
@@ -1,8 +1,60 @@
 import React from "react";
+import axios from "axios";
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  Row,
+  Col,
+  Container
+} from "reactstrap";
 
 class FoodtruckList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      foodtrucks: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://ftruck-api.herokuapp.com/api/foodTrucks/")
+      .then(result => {
+        this.setState({ foodtrucks: result.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
-    return <React.Fragment>foodtrucklist</React.Fragment>;
+    const { foodtrucks } = this.state;
+
+    const foodtruckList = foodtrucks.map(foodtruck => {
+      return (
+        <Col key={foodtruck._id}>
+          <Card>
+            <CardBody>
+              <CardTitle>{foodtruck.vendorName}</CardTitle>
+              <CardSubtitle>{foodtruck.foodType}</CardSubtitle>
+              <CardText>Website: {foodtruck.metadata.url}</CardText>
+            </CardBody>
+          </Card>
+        </Col>
+      );
+    });
+    return (
+      <React.Fragment>
+        <Container>
+          <h1 className="text-center">Foodtrucks</h1>
+          <Row>{foodtruckList}</Row>
+        </Container>
+      </React.Fragment>
+    );
   }
 }
 
